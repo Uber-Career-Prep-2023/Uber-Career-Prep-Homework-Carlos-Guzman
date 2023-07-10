e94eaa10be79a006aeb1dd0133fc9da551af10c0
charlyguz <gusmanc622@gmail.com>
2023-07-10 14:25:59 -0600

    all problems have been corrected :)

--- Assignment-3/07_ReverseWords.py
+++ Assignment-3/07_ReverseWords.py
@@ -7,48 +7,98 @@ Space: O(N) (Storing split words and reversed string)
 Technique: String manipulation.
 Time: ~25 minutes.
 """
-def ReverseWords(string):
-    reversed_string = ''  # Variable to store the reversed string
-    splitted_string = []  # List to store individual words
-    crr_word = ''  # Variable to hold the current word being constructed
+# def ReverseWords(string):
+#     reversed_string = ''  # Variable to store the reversed string
+#     splitted_string = []  # List to store individual words
+#     crr_word = ''  # Variable to hold the current word being constructed
+
+#     # Iterate through each character in the input string
+#     for i in string:
+#         if i == ' ':
+#             splitted_string.append(crr_word)  # Add the current word to the list
+#             crr_word = ''  # Reset the current word
+#         crr_word += i  # Add the current character to the current word
+#     splitted_string.append(crr_word)  # Add the last word to the list
+
+#     # Iterate through the reversed list of words
+#     for j in reversed(splitted_string):
+#         reversed_string += j  # Concatenate the reversed words
+#         reversed_string += ' '  # Add a space after each word
+
+# #     return reversed_string
+# # Test cases
+# # Case 1: Basic example
+# input_str = "Uber Career Prep"
+# # Output: "Prep Career Uber"
+# print(ReverseWords(input_str))
+
+# # Case 2: Example with punctuation and multiple spaces
+# input_str = "Emma lives in Brooklyn, New York."
+# # Output: "York. New Brooklyn, in lives Emma"
+# print(ReverseWords(input_str))
+
+# # Case 3: Empty string
+# input_str = ""
+# # Output: ""
+# print(ReverseWords(input_str))
+
+# # Case 4: Single word
+# input_str = "Hello"
+# # Output: "Hello"
+# print(ReverseWords(input_str))
+
+# # Case 5: String with leading and trailing spaces
+# input_str = "   Uber is amazing!   "
+# # Output: "   amazing! is    "
+# print(ReverseWords(input_str))
+
+
+
+def ReverseWordsStack(string):
+    reversed_string = ''  # Initialize an empty string to hold the reversed string
+    stack = []  # Initialize an empty list to act as a stack to hold individual words
+    word = ''  # Initialize an empty string to hold the current word being constructed
 
     # Iterate through each character in the input string
-    for i in string:
-        if i == ' ':
-            splitted_string.append(crr_word)  # Add the current word to the list
-            crr_word = ''  # Reset the current word
-        crr_word += i  # Add the current character to the current word
-    splitted_string.append(crr_word)  # Add the last word to the list
+    for char in string:
+        if char == ' ':  # If the current character is a space
+            stack.append(word)  # Add the current word to the stack
+            word = ''  # Reset the current word to an empty string
+        else:
+            word += char  # If the current character is not a space, add it to the current word
 
-    # Iterate through the reversed list of words
-    for j in reversed(splitted_string):
-        reversed_string += j  # Concatenate the reversed words
-        reversed_string += ' '  # Add a space after each word
+    # Add the last word to the stack
+    stack.append(word)
 
-    return reversed_string
+    # While the stack is not empty
+    while stack:
+        # Pop the top word off the stack and add it to the reversed string
+        reversed_string += stack.pop()
+        if stack:  # If there are still words in the stack
+            reversed_string += ' '  # Add a space after each word (except for the last word)
 
-# Test cases
-# Case 1: Basic example
-input_str = "Uber Career Prep"
-# Output: "Prep Career Uber"
-print(ReverseWords(input_str))
+    return reversed_string  # Return the reversed string
+# Test case 1: Basic example
+input_str = "Hello, my name is Carlos Guzman"
+# Expected output: "Guzman Carlos is name my Hello,"
+print(ReverseWordsStack(input_str))
 
-# Case 2: Example with punctuation and multiple spaces
-input_str = "Emma lives in Brooklyn, New York."
-# Output: "York. New Brooklyn, in lives Emma"
-print(ReverseWords(input_str))
-
-# Case 3: Empty string
-input_str = ""
-# Output: ""
-print(ReverseWords(input_str))
-
-# Case 4: Single word
+# Test case 2: Single word
 input_str = "Hello"
-# Output: "Hello"
-print(ReverseWords(input_str))
+# Expected output: "Hello"
+print(ReverseWordsStack(input_str))
 
-# Case 5: String with leading and trailing spaces
-input_str = "   Uber is amazing!   "
-# Output: "   amazing! is    "
-print(ReverseWords(input_str))
+# Test case 3: Empty string
+input_str = ""
+# Expected output: ""
+print(ReverseWordsStack(input_str))
+
+# Test case 4: String with leading and trailing spaces
+input_str = "   Hello, world!   "
+# Expected output: "   world! Hello,"
+print(ReverseWordsStack(input_str))
+
+# Test case 5: String with multiple spaces between words
+input_str = "Hello,   world!"
+# Expected output: "world!   Hello,"
+print(ReverseWordsStack(input_str))
--- Assignment-3/09_MergeKSortedArrays.py
+++ Assignment-3/09_MergeKSortedArrays.py
@@ -6,31 +6,51 @@ Technique: Heap-based Merge
 Time: ~40 minutes.
 """
 
-import math
-
 class Heap:
     def __init__(self, heap=None):
         self.min_heap = heap if heap is not None else []  # Initialize the min-heap with the given heap array
-    
+        for i in reversed(range(len(self.min_heap)//2)):
+            self.heapify(i)
+    #another error was not verifying if the list of values entered was a heap and not creating the heapify function.
+    def heapify(self, index):
+        heap = self.min_heap
+        left = (2 * index) + 1  # Index of the left child
+        right = (2 * index) + 2  # Index of the right child
+        smallest = index
+
+        # Check if the left child exists and if it is smaller than the current node
+        if left < len(heap) and heap[left] < heap[smallest]:
+            smallest = left
+        # Check if the right child exists and if it is smaller than the current node
+        if right < len(heap) and heap[right] < heap[smallest]:
+            smallest = right
+        if smallest != index:
+            # If either of the children is smaller than the current node, swap them
+            heap[smallest], heap[index] = heap[index], heap[smallest]
+            self.heapify(smallest)
     def top(self):
+        if not self.min_heap:
+            return None
         # Return the minimum element of the min-heap (element at index 0)
         return self.min_heap[0] if self.min_heap else None
     
     def insert(self, value):
         heap = self.min_heap
         heap.append(value)  # Add the value to the end of the heap
-        parent = math.floor(((len(heap) - 1) / 2))  # Calculate the index of the parent of the newly added value
+        parent = ((len(heap) - 1) // 2)  # Calculate the index of the parent of the newly added value
         position_val = len(heap) - 1  # Position of the newly added value
 
-        while heap[position_val] < heap[parent]:
+        while position_val > 0 and heap[position_val] < heap[parent]:
             # Swap the value with its parent if it is smaller
             heap[position_val], heap[parent] = heap[parent], heap[position_val]
             position_val = parent
-            parent = math.floor(((position_val - 1) / 2))  # Calculate the index of the new parent
+            parent = ((position_val - 1) // 2)  # Calculate the index of the new parent
         return position_val
     
     def remove(self):
         heap = self.min_heap
+        if not heap:
+            return None
         if len(heap) == 0:
             return None
         val = heap[0]  # Store the minimum value (root of the heap)
@@ -39,8 +59,8 @@ class Heap:
         crr_position = 0
 
         while True:
-            left_child = math.floor((2 * crr_position) + 1)  # Index of the left child
-            right_child = math.floor((2 * crr_position) + 2)  # Index of the right child
+            left_child = (2 * crr_position) + 1  # Index of the left child
+            right_child = (2 * crr_position) + 2  # Index of the right child
             min_index = crr_position
 
             if left_child < len(heap) and heap[min_index] > heap[left_child]:
@@ -49,9 +69,9 @@ class Heap:
             if right_child < len(heap) and heap[min_index] > heap[right_child]:
                 # If the right child is smaller, update the minimum index
                 min_index = right_child
-            heap[crr_position], heap[min_index] = heap[min_index], heap[crr_position]
             if crr_position == min_index:
                 break
+            heap[crr_position], heap[min_index] = heap[min_index], heap[crr_position] #Here was the error: an exchange was being made even if the current position is equal to min_index.
             crr_position = min_index
         return val
 
@@ -66,7 +86,7 @@ def MergeKSortedArrays(arrays):
             heap.insert(j)  # Insert the element into the heap
 
     # Extract the smallest element from the heap until it becomes empty
-    while heap:
+    while heap.min_heap:
         sorted_arr.append(heap.remove())  # Remove the smallest element and append it to the sorted array
 
     return sorted_arr  # Return the merged and sorted array
